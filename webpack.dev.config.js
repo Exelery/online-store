70eba03b1526915c036da3a24a07ac6f2939201d
Owner <test>
2022-12-14 22:47:54 +0300

    fix: webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,6 +6,8 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, '../dist'),
+    static: {
+      directory: path.resolve(__dirname, '../dist'),
+    },
   },
 };
