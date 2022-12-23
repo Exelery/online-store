26f5b9b9f151300529bb7a5b8cd6bc893c16906a
Owner <test>
2022-12-23 15:57:55 +0300

    feat: add types and loader

---
+++ src/components/app/app.ts
@@ -0,0 +1,9 @@
+// import {} from '../../utils/types';
+export default class App {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private view: any;
+
+  public start() {
+    const content = document.querySelector('main-page__content') as Element;
+  }
+}
