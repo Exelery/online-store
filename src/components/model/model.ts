b423c4f7af448d87c08c22a177a4a697350b6969
Owner <test>
2022-12-26 21:01:25 +0300

    feat: add more functions in model

--- src/components/model/model.ts
+++ src/components/model/model.ts
@@ -1,12 +1,14 @@
 import { ApiLoader } from '../../loader/loader';
-import { IData, IProduct } from '../../utils/types';
+import { IData, IProduct, SortParm } from '../../utils/types';
 
 export default class Model {
   private apiLoader: ApiLoader;
   private productsAll: IProduct[];
+  public shoppingCart: { id: number; count: number; price: number }[] | null;
   constructor() {
     this.apiLoader = new ApiLoader();
     this.loadData();
+    this.shoppingCart = this.getShoppingCart();
   }
 
   async loadData() {
@@ -16,7 +18,7 @@ export default class Model {
   }
 
   public filterByRange(field: 'price' | 'stock', minValue: number, maxValue: number): IProduct[] {
-    return this.productsAll.filter((item) => item[field] >= minValue && item[field] < minValue);
+    return this.productsAll.filter((item) => item[field] >= minValue && item[field] <= maxValue);
   }
 
   public filterByField(field: 'category' | 'brand', value: string): IProduct[] {
@@ -33,7 +35,34 @@ export default class Model {
     );
   }
 
-  // public drawItemCards(item: IProduct[]){
+  public sortItems(dir: SortParm, arr: IProduct[]) {
+    switch (dir) {
+      case SortParm.priceDown:
+        return arr.sort((el1, el2) => el2.price - el1.price);
+        break;
+      case SortParm.priceUp:
+        return arr.sort((el1, el2) => el1.price - el2.price);
+        break;
+      case SortParm.ratingDown:
+        return arr.sort((el1, el2) => el2.rating - el1.rating);
+        break;
+      case SortParm.ratingUp:
+        return arr.sort((el1, el2) => el1.rating - el2.rating);
+        break;
+    }
+  }
 
-  // }
+  getShoppingCart() {
+    const storage: string | null = localStorage.getItem('shopping');
+    if (storage) {
+      return JSON.parse(storage);
+    }
+    return null;
+    // public drawItemCards(item: IProduct[]){
+    // }
+  }
+
+  saveShoppingCart() {
+    localStorage.setItem('shopping', JSON.stringify(this.shoppingCart));
+  }
 }
