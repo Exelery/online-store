db76c1607838154cfdbe2d58b709c22b7a1e22bb
Owner <test>
2022-12-26 20:59:43 +0300

    fix: fix router on dev server

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,6 +6,7 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    historyApiFallback: true,
     static: {
       directory: path.resolve(__dirname, '../dist'),
     },
